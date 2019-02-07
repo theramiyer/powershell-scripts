d83804deb89eb71eced8863179c498001d22bf0b
Ram Iyer <ramakrishnan.rkology@gmail.com>
2019-02-07 12:07:54 -0600

    Complete the service check script

--- Windows/Get-ServiceStatus.ps1
+++ Windows/Get-ServiceStatus.ps1
@@ -1,3 +1,22 @@
+function main {
+    Get-ServiceStatus -InputFilePath '\\path\to\input-file.csv' -From 'bot@domain.com' -SmtpServer 'smtp@domain.com'
+
+    <#
+        The input file should have three columns, named, "ComputerName", "Service" and "NotificationEmail". You can have more columns if you want to use the same file for different purposes; this script will use only those three columns.
+
+        If you need multiple services to be monitored on a single server, make multiple entries. Like so:
+
+        SVR001  |  WinRM  | someone@domain.com
+        SVR001  |  Store  | someone@domain.com;another@domain.com
+
+        Service should contain the service name (NOT the display name).
+
+        NotificationEmail can have multiple addresses, a semicolon (;) should be used to split the addresses. Like so: me@domain.com;you@domain.com;they@domain.com.
+
+        DO NOT use quotes anywhere in the input file. It can confuse the script.
+    #>
+}
+
 function New-InputFile {
     param (
         # Path to the file
@@ -27,7 +46,12 @@ function Get-ServiceStatus {
         # From address, from which the email should be sent
         [Parameter(Mandatory=$true)]
         [string]
-        $From
+        $From,
+
+        # SMTP server FQDN
+        [Parameter(Mandatory=$true)]
+        [string]
+        $SmtpServer
     )
 
     begin {
@@ -67,11 +91,36 @@ function Get-ServiceStatus {
                 break
             }
         }
+
+        $style = "<style>BODY{font-family:'Segoe UI';font-size:10pt;line-height: 120%}h1,h2{font-family:'Segoe UI Light';font-weight:normal;}TABLE{border:1px solid white;background:#f5f5f5;border-collapse:collapse;}TH{border:1px solid white;background:#f0f0f0;padding:5px 10px 5px 10px;font-family:'Segoe UI Light';font-size:13pt;font-weight: normal;}TD{border:1px solid white;padding:5px 10px 5px 10px;}</style>"
+
+        $ServiceStatusTable = @()
     }
 
     process {
         foreach ($Record in $ServiceRecords) {
-            Get-Service -Name $Record.Service -ComputerName $Record.ComputerName
+            $ServiceStatus = Get-Service -Name $Record.Service -ComputerName $Record.ComputerName
+
+            $ServiceStatusRecord = [ordered]@{
+                ComputerName = $Record.ComputerName
+                ServiceName = $Record.Service
+                Status = $ServiceStatus.Status
+                NotificationEmail = $Record.NotificationEmail
+            }
+
+            $ServiceStatusTable += $ServiceStatusRecord
+        }
+
+        $StoppedServices = $ServiceStatusTable | Where-Object Status -ne 'Running' | Group-Object NotificationEmail
+
+        foreach ($Group in $StoppedServices) {
+            $StoppedServices = $Group.Group | Select-Object ComputerName, ServiceName, Status | ConvertTo-Html -As Table -Fragment | Out-String
+
+            $Body = ConvertTo-Html -Head $style -Body '<p>Hi Team,</p><p>The following services were found to be not running when the status was checked by the Automated Service Status Check monitor.</p>', $StoppedServices, '<p>Please take actions as necessary.</p><p>Thanks,<br />Service Check Bot</p>' | Out-String
+
+            Send-MailMessage -From $From -To ($Group.Name -split ';').Trim() -SmtpServer $SmtpServer -Subject 'Services found to be not running' -Body $Body -BodyAsHtml
         }
     }
 }
+
+main
